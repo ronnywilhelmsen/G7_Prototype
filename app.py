03458ab7e234187fef06168cdd8d6185e4d47d41
Kubilay Kerim <kubioz@icloud.com>
2021-10-07 13:30:51 +0200

    Simple page structure for marketplace

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! Kubi was here¨'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+
+    return render_template('market.html', items=items)
+
+@app.route('/market2')
+def market_page2():
+    items2 = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+
+    return render_template('market2.html', items2=items2)
 
 
-if __name__ == '__main__':
-    app.run()
