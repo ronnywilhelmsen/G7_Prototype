8d2a35b2c1483a39dac823942d1591cd8d2ba1e8
Kubilay Kerim <kubioz@icloud.com>
2021-10-25 22:20:13 +0200

    Major implementation of backend functions and project structure

--- app.py
+++ app.py
@@ -1,31 +1,7 @@
-from flask import Flask,render_template
+from web import start
 
-app = Flask(__name__)
+app = start()
 
-
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/market')
-def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-
-    return render_template('market.html', items=items)
-
-@app.route('/market2')
-def market_page2():
-    items2 = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-
-    return render_template('market2.html', items2=items2)
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
---
+++ web/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+database = SQLAlchemy()
+
+Database_Name = "database.db"
+
+def start():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "VerySecret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{Database_Name}'
+
+    database.init_app(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    start_database(app)
+
+    from .models import Category, Item
+
+    return app
+
+def start_database(app):
+    if not path.exists("web/" + Database_Name):
+        database.create_all(app=app)
